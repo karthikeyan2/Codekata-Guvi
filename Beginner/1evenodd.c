d2631e18f7dec9b75acddc253955b6dd8f8397d2
Nivetha Manikandan <34644928+NivethaM19@users.noreply.github.com>
2017-12-19 19:17:35 +0530

    Create 1evenodd.c

---
+++ Beginner/1evenodd.c
@@ -0,0 +1,16 @@
+#include <stdio.h>
+
+int main(void) 
+{
+	int n;
+	scanf("%d",&n);
+	if(n%2==0)
+	{
+		printf"\n the number is even");
+	}
+	else
+	{
+		printf("\n the number is odd");
+	}
+	return 0;
+}
