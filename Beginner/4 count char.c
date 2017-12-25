24c164d485d9e0186ea39cc06d40405cb135ac1a
Nivetha Manikandan <34644928+NivethaM19@users.noreply.github.com>
2017-12-25 11:59:19 +0530

    Create 4 count char.c

---
+++ Beginner/4 count char.c
@@ -0,0 +1,18 @@
+#include <stdio.h>
+#include<string.h>
+int main(void) {
+	char a[50];
+	int i,count=0,m;
+	scanf("%s",a);
+	m=strlen(a);
+	for(i=0;i<m;i++)
+	{
+		if(a[i]!=' ')
+		{
+			count++;
+		}
+	continue;	
+	}
+	printf("%d",count);
+	return 0;
+}
