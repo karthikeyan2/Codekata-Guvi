ba99aefbfe5ea9a97398acc2320419eacea83421
Nivetha Manikandan <34644928+NivethaM19@users.noreply.github.com>
2017-12-26 20:40:18 +0530

    Create 5 number num.c

---
+++ Beginner/5 number num.c
@@ -0,0 +1,16 @@
+#include <stdio.h>
+
+int main(void) {
+	char a[10];
+	int i,count=0;
+	scanf("%s",a);
+	for(i=0;a[i]!='\0';i++)
+	{
+		if(a[i]>='0' && a[i]<='9')
+		{
+			count=count+1;
+		}
+	}
+	printf("%d",count);
+	return 0;
+}
