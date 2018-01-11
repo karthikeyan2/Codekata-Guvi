2ea82d8585c9053782ea1ef0c822c4f7d30843a0
Nivetha Manikandan <34644928+NivethaM19@users.noreply.github.com>
2018-01-11 10:48:05 +0530

    Create 3 numericstring.c

---
+++ Player/3 numericstring.c
@@ -0,0 +1,26 @@
+#include <stdio.h>
+
+int main(void) {
+	char a[10];
+	scanf("%s",a);
+	int i,flag=0;
+	for(i=0;a[i]!='\0';i++)
+	{
+		if(a[i]>='0' && a[i]<='9')
+		{
+			flag=1;
+		}
+		else
+		{
+			flag=0;
+			break;
+		}
+	}
+	if(flag==1)
+	{
+		printf("yes");
+	}
+	else
+	printf("no");
+	return 0;
+}
