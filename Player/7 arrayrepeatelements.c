c7de604289f4992da042b32d834d90980e78132b
Nivetha Manikandan <34644928+NivethaM19@users.noreply.github.com>
2018-01-18 16:10:12 +0530

    Create 7 arrayrepeatelements.c

---
+++ Player/7 arrayrepeatelements.c
@@ -0,0 +1,29 @@
+#include <stdio.h>
+
+int main(void) {
+	int n,k,count=1;
+	scanf("%d %d",&n,&k);
+	int a[n];
+	int i,j;
+	for(i=0;i<n;i++)
+	{
+		scanf("%d",&a[i]);
+	}
+	for(i=0;i<n;i++)
+	{
+		for(j=i+1;j<n;j++)
+		{
+			if(a[i]==a[j])
+			{
+				count=count+1;
+				
+			}
+		}
+		if(count==k)
+		{
+			printf("%d",a[i]);
+			break;
+		}
+	}
+return 0;
+}
